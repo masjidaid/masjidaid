11968ee44da377fc18901eff853eea17ff4cf221
Jubril Issa <issa.jubril@andela.com>
2020-02-26 19:48:26 +0100

    Add campaign app to project

---
+++ masjidaid/campaign/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.conf import settings
+
+
+class Campaign(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    target = models.DecimalField(decimal_places=2, max_digits=8)
+    is_active = models.BooleanField(default=False)
