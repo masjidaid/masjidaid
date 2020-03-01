5ee14e904c3af3d2852a125803893942a6b4ee8b
Jubril Issa <issa.jubril@andela.com>
2020-03-01 19:58:20 +0100

    Add campaign to the base project schema

--- masjidaid/schema.py
+++ masjidaid/schema.py
@@ -2,13 +2,14 @@ import graphene
 import graphql_jwt
 
 import masjidaid.accounts.schema
+import masjidaid.campaign.schema
 
 
-class Query(masjidaid.accounts.schema.Query, graphene.ObjectType):
+class Query(masjidaid.accounts.schema.Query, masjidaid.campaign.schema.Query, graphene.ObjectType):
     pass
 
 
-class Mutation(masjidaid.accounts.schema.Mutation, graphene.ObjectType,):
+class Mutation(masjidaid.accounts.schema.Mutation, masjidaid.campaign.schema.Mutation, graphene.ObjectType,):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
