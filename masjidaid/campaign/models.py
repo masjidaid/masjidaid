b74ec1de0ffe2b300a2e7188e4302aaa0834d7f5
Jubril Issa <issa.jubril@andela.com>
2020-03-01 19:57:30 +0100

    Use integer fields for amount

--- masjidaid/campaign/models.py
+++ masjidaid/campaign/models.py
@@ -6,5 +6,6 @@ class Campaign(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    target = models.DecimalField(decimal_places=2, max_digits=8)
+    target = models.IntegerField()
+    # target = models.DecimalField(decimal_places=2, max_digits=8)
     is_active = models.BooleanField(default=False)
--- masjidaid/transaction/models.py
+++ masjidaid/transaction/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 class Transaction(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    amount = models.DecimalField(default=Decimal("0.0"))    
+    amount = models.IntegerField()
+    # amount = models.DecimalField(default=Decimal("0.0"), max_digits=12, decimal_places=2)
     campaign = models.ForeignKey('campaign.Campaign', on_delete=models.CASCADE)
     donor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    
